71b4749e7203d77b501a4a843c819fa6db9e1925
Jason Belt <belt@ksu.edu>
2023-12-14 12:59:53 -0600

    update jenkins script

--- rts/hamr/slang/bin/jenkinsScript-rts1.cmd
+++ rts/hamr/slang/bin/jenkinsScript-rts1.cmd
@@ -63,6 +63,7 @@ assert(results.ok)
 
 val lines = ops.StringOps(results.out ).split(c => c == '\n')
 
+var runners: ISZ[ST] = ISZ()
 var commands : ISZ[ST] = ISZ()
 var i = 0
 while(i < lines.size) {
@@ -88,16 +89,23 @@ while(i < lines.size) {
 
   val temp = Os.temp()
   temp.writeOver(json.s)
-  commands = commands :+ st"""DSC_RUNNER_DIR = s"${dsc_runner_dir.string}""""
-  commands = commands :+ st"""DSC_TESTER_DIR = s"${dsc_tester_dir.string}""""
-  commands = commands :+ st"""// create the result directories for ${testFamilyName} on the test server and upload the json file"""
-  commands = commands :+ st"""proc"ssh ${proxyJump} santos_jenkins@${testServer} mkdir -p $${DSC_RUNNER_DIR}".echo.console.runCheck()"""
-  commands = commands :+ st"""proc"ssh ${proxyJump} santos_jenkins@${testServer} mkdir -p $${DSC_TESTER_DIR}".echo.console.runCheck()"""
-  commands = commands :+ st"""proc"scp $proxyJump ${temp} santos_jenkins@${testServer}:${dsc_runner_dir.up}/testRow.json".echo.runCheck()"""
-  commands = commands :+ st"""proc"scp $proxyJump ${temp} santos_jenkins@${testServer}:${dsc_tester_dir.up}/testRow.json".echo.runCheck()"""
-  commands = commands :+ st"""// trigger ${testFamilyName} tests on jenkins"""
-  commands = commands :+ st"""proc"$$CURL_PREFIX ${jenkinsArgs} --data DSC_RUNNER_DIR=$${DSC_RUNNER_DIR} --data DSC_TESTER_DIR=$${DSC_TESTER_DIR} --data DSC_JAR_LOC=$jarFileDest --data DSC_TEST_SERVER=${testServer} --data DSC_PREFIX=${DSC_PREFIX}".echo.console.runCheck()"""
-  commands = commands :+ st""
+
+  runners = runners :+ st"val run_${testFamilyName}: B = T"
+
+  commands = commands :+
+    st"""if(run_${testFamilyName}) {
+        |  // ${testFamilyName}
+        |  DSC_RUNNER_DIR = s"${dsc_runner_dir.string}"
+        |  DSC_TESTER_DIR = s"${dsc_tester_dir.string}"
+        |  // create the result directories for ${testFamilyName} on the test server and upload the json file
+        |  proc"ssh ${proxyJump} santos_jenkins@${testServer} mkdir -p $${DSC_RUNNER_DIR}".echo.console.runCheck()
+        |  proc"ssh ${proxyJump} santos_jenkins@${testServer} mkdir -p $${DSC_TESTER_DIR}".echo.console.runCheck()
+        |  proc"scp $proxyJump ${temp} santos_jenkins@${testServer}:${dsc_runner_dir.up}/testRow.json".echo.runCheck()
+        |  proc"scp $proxyJump ${temp} santos_jenkins@${testServer}:${dsc_tester_dir.up}/testRow.json".echo.runCheck()
+        |  // trigger ${testFamilyName} tests on jenkins
+        |  proc"$$CURL_PREFIX ${jenkinsArgs} --data DSC_RUNNER_DIR=$${DSC_RUNNER_DIR} --data DSC_TESTER_DIR=$${DSC_TESTER_DIR} --data DSC_JAR_LOC=$jarFileDest --data DSC_TEST_SERVER=${testServer} --data DSC_PREFIX=${DSC_PREFIX}".echo.console.runCheck()
+        |}
+        |"""
 
   i = i + 2
 }
@@ -114,6 +122,8 @@ val batch =
       |val CURL_PREFIX=s"$curlPrefix"
       |var DSC_RUNNER_DIR=""
       |var DSC_TESTER_DIR=""
+      |
+      |${(runners, "\n")}
       |
       |${(commands, "\n")}
       |"""
